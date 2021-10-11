07db8a92ae38bcd5561cf2cc599ec694c1601a30
raychenon <raymond.chenon@gmail.com>
2021-10-12 00:02:49 +0200

    init

---
+++ src/main/java/com/raychenon/s3/Application.java
@@ -0,0 +1,9 @@
+package com.raychenon.s3;
+
+public class Application {
+
+    public static void main(String[] args){
+
+          System.out.println("Bla");
+    }
+}
---
+++ src/main/java/com/raychenon/s3/S3GetObject.java
@@ -0,0 +1,41 @@
+package com.raychenon.s3;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class S3GetObject {
+
+    public void read(String bucket_name, String key_name){
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        try {
+            S3Object o = s3.getObject(bucket_name, key_name);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(new File(key_name));
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                fos.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            fos.close();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
